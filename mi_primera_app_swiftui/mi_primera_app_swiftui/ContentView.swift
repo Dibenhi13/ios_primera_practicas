a77ee64ad17acc2d018f626c592ff4fe2007172b
alumno <alumno@PC-V202-19.uacj.mx>
2025-02-21 10:41:14 -0700

    [PRIMER] commit para iniciar el repositorio

---
+++ mi_primera_app_swiftui/mi_primera_app_swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mi_primera_app_swiftui
+//
+//  Created by alumno on 2/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
